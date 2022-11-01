deedf6592d6d20fb824461da7de42d88f992909f
Sai Manoj Chamarti <saimanojchamarti@gmail.com>
2022-11-01 11:43:53 +0530

    adding project

---
+++ src/main/java/com/sbj/helloworld/HelloWorldApplication.java
@@ -0,0 +1,27 @@
+package com.sbj.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String get() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/{name}")
+	public String getName(@PathVariable("name") String name) {
+		return "Hello " + name;
+	}
+
+}
